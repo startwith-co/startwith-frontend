870497ca21c6a8e5aebb199bda00618cdfc091bd
InGwonKang <tnqkr3494@naver.com>
2025-04-30 23:48:49 +0900

    [no-issue] fix: chat layout 오타 수정

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import Header from '@/shared/ui/header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="2xl:px-[300px]P h-screen bg-[rgba(250,252,255,1)] px-4 sm:px-8 md:px-16 lg:px-32">
+    <div className="h-screen bg-[rgba(250,252,255,1)] px-4 sm:px-8 md:px-16 lg:px-32 2xl:px-[300px]">
       <Header />
       {children}
     </div>
