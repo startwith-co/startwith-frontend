7f8a7f6637bac339ebbff1f690831193bf697ece
InGwonKang <tnqkr3494@naver.com>
2025-05-01 21:24:18 +0900

    [#7] feat: pretendard 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,49 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import localFont from 'next/font/local';
 import AmplitudeContextProvider from './_providers/amplitude-provider';
 import SentryProvider from './_providers/sentry-provider';
+
+const PretendardFont = localFont({
+  src: [
+    {
+      path: '../shared/fonts/Pretendard-Black.woff2',
+      weight: '900',
+    },
+    {
+      path: '../shared/fonts/Pretendard-ExtraBold.woff2',
+      weight: '800',
+    },
+    {
+      path: '../shared/fonts/Pretendard-Bold.woff2',
+      weight: '700',
+    },
+    {
+      path: '../shared/fonts/Pretendard-SemiBold.woff2',
+      weight: '600',
+    },
+    {
+      path: '../shared/fonts/Pretendard-Medium.woff2',
+      weight: '500',
+    },
+    {
+      path: '../shared/fonts/Pretendard-Regular.woff2',
+      weight: '400',
+    },
+    {
+      path: '../shared/fonts/Pretendard-Light.woff2',
+      weight: '300',
+    },
+    {
+      path: '../shared/fonts/Pretendard-ExtraLight.woff2',
+      weight: '200',
+    },
+    {
+      path: '../shared/fonts/Pretendard-Thin.woff2',
+      weight: '100',
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,7 +57,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="kr">
-      <body className="vsc-initialized">
+      <body className={`${PretendardFont.className}`}>
         <AmplitudeContextProvider userId="">
           {process.env.NEXT_PUBLIC_NODE_ENV === 'development' ? (
             <> {children}</>
