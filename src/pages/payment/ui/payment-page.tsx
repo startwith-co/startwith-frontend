5e17c6600344d165b7f89f6a3d1d9eeadf5557df
InGwonKang <tnqkr3494@naver.com>
2025-05-01 17:26:44 +0900

    [#7] feat: footer ui 제작

---
+++ src/pages/payment/ui/payment-page.tsx
@@ -0,0 +1,11 @@
+export default function PaymentPage() {
+  return (
+    <div>
+      <div>
+        <div>주문 내역</div>
+        <div>총 결제 금액</div>
+      </div>
+      <div>SOLU 포인트/쿠폰</div>
+    </div>
+  );
+}
