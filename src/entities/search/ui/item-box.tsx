7d2d13d8339ddc3e675be41dd9034a5ff6e0dc96
InGwonKang <tnqkr3494@naver.com>
2025-06-03 18:11:47 +0900

    [#66] fix: search sidebar 수정

--- src/entities/search/ui/item-box.tsx
+++ src/entities/search/ui/item-box.tsx
@@ -12,8 +12,8 @@ export default function ItemBox({
     <Link href="/products/1" className="flex flex-col gap-5">
       <div className="h-56 w-full rounded-md bg-gray-200" />
       <div className="flex flex-col gap-1">
-        <span className="font-bold">{name}</span>
-        <span>{price}</span>
+        <span className="text-[15px] font-bold">{name}</span>
+        <span className="text-[15px]">{price}</span>
         <p className="flex items-center gap-1">
           <IoIosStar />
           <span className="text-xs">{rating}</span>
