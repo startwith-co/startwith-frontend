cc446f68e8fe6276e38d67fa2feaabc65329fd7b
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-06-19 00:07:35 +0900

    [#87] feat: 비밀먼호 재설성  ui 수정 및 로직 수정

--- src/features/reset/api/resetPost.tsx
+++ src/features/reset/api/resetPost.tsx
@@ -1,10 +1,49 @@
 'use server';
 
-async function resetPost(_prevState: void, formData: FormData): Promise<void> {
+import ky from '@/shared/api/server-api';
+
+async function resetPost(
+  _prevState: void,
+  formData: FormData,
+  user: string,
+  token: string,
+): Promise<void> {
   const password = formData?.get('password') as string;
   const confirmPassword = formData?.get('confirmPassword') as string;
-  if (!password || password.trim().length === 0) return;
-  console.log(password, confirmPassword);
+
+  if (
+    !password ||
+    password.trim().length === 0 ||
+    confirmPassword ||
+    confirmPassword.trim().length === 0 ||
+    user ||
+    user.trim().length === 0 ||
+    token ||
+    token.trim().length === 0
+  )
+    return;
+
+  const option = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    json: {
+      newPassword: password,
+      confirmPassword,
+    },
+  };
+
+  if (user === 'vendor') {
+    await ky.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/b2b-service/vendor/resetPassword`,
+      option,
+    );
+  } else {
+    await ky.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/b2b-service/consumer/resetPassword`,
+      option,
+    );
+  }
 }
 
 export default resetPost;
