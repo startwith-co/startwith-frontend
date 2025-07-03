707ed4b238e7304ec3911ba73d082558a46687c8
InGwonKang <tnqkr3494@naver.com>
2025-07-03 12:29:18 +0900

    [#107] feat: 벤더 정산 관리 페이지 table api 연동

---
+++ src/views/vendorCalculate/api/getVendorTable.ts
@@ -0,0 +1,35 @@
+import api from '@/shared/api/index-api';
+import { ApiResponse } from '@/shared/model/apiType';
+import VendorTableProps from '../model/type';
+
+export default async function getVendorTable(
+  vendorSeq: string,
+  paymentStatus: string,
+  start: string,
+  end: string,
+) {
+  let paymentStatusParam = '';
+  switch (paymentStatus) {
+    case '전체':
+      paymentStatusParam = '';
+      break;
+    case '정산 대기':
+      paymentStatusParam = '&paymentStatus=DONE';
+      break;
+    case '정산 완료':
+      paymentStatusParam = '&paymentStatus=SETTLED';
+      break;
+    default:
+      paymentStatusParam = '';
+      break;
+  }
+  const startParam = start ? `&start=${start}` : '';
+  const endParam = end ? `&end=${end}` : '';
+  const res = await api
+    .get(
+      `api/b2b-service/dashboard/vendor?vendorSeq=${vendorSeq}${paymentStatusParam}${startParam}${endParam}`,
+    )
+    .json<ApiResponse<VendorTableProps[]>>();
+
+  return res;
+}
--- src/widgets/vendorHome/ui/vendor-board.tsx
+++ src/widgets/vendorHome/ui/vendor-board.tsx
@@ -16,10 +16,12 @@ export default async function VendorBoard({
         운영 현황
       </h2>
       <div className="grid w-full grid-cols-4 justify-between gap-1 sm:gap-2 md:gap-3 lg:gap-4">
-        {['상담 대기', '구매확정', '정산대기', '정산완료'].map((label) => (
+        {['상담 대기', '구매 확정', '정산 대기', '정산 완료'].map((label) => (
           <div key={label} className="flex flex-col items-center">
             <DarkBox className="xs:min-w-[2.5rem] xs:max-w-[3rem] flex aspect-square w-full max-w-[2.5rem] min-w-[2rem] items-center justify-center text-xl sm:max-w-[4rem] sm:min-w-[3rem] sm:text-2xl md:max-w-[5rem] md:min-w-[3.5rem] md:text-3xl 2xl:max-w-[5.625rem] 2xl:min-w-[4rem] 2xl:text-4xl">
-              {boardCount.data[switchBoardLabel(label) as keyof BoardCountType]}
+              {boardCount.data[
+                switchBoardLabel(label) as keyof BoardCountType
+              ] || 0}
             </DarkBox>
             <span className="xs:text-[0.6rem] mt-1 text-[0.5rem] sm:text-xs 2xl:text-sm">
               {label}
