2d615515d19d0c342e148d0bd52267b28b79f1d2
InGwonKang <tnqkr3494@naver.com>
2025-07-07 14:47:10 +0900

    [#114] feat: 임시 실시간 상담 대기 카운트

--- src/widgets/vendorHome/api/getBoardCount.ts
+++ src/widgets/vendorHome/api/getBoardCount.ts
@@ -1,9 +1,9 @@
-import serverApi from '@/shared/api/server-api';
+import api from '@/shared/api/index-api';
 import { ApiResponse } from '@/shared/model/apiType';
 import BoardCountType from '../model/type';
 
 export default async function getBoardCount(vendorSeq: number) {
-  const res = await serverApi<ApiResponse<BoardCountType>>(
+  const res = await api<ApiResponse<BoardCountType>>(
     `api/b2b-service/dashboard/vendor/status?vendorSeq=${vendorSeq}`,
   ).json();
 
--- src/widgets/vendorHome/ui/vendor-chat-widget.tsx
+++ src/widgets/vendorHome/ui/vendor-chat-widget.tsx
@@ -1,15 +1,29 @@
+'use client';
+
+import useGetChatRooms from '@/shared/model/useGetChatRooms';
 import DarkBox from '@/shared/ui/dark-box';
 
 export default function VendorChatWidget() {
+  const rooms = useGetChatRooms({ targetId: 'vendorId' });
   return (
     <div className="rounded-2xl bg-white p-7 shadow-md">
       <h2 className="font-semibold 2xl:text-xl">실시간 상담 현황</h2>
       <ul className="mt-5 flex flex-col gap-5">
-        <li>
-          <DarkBox className="w-full py-7 text-center text-xs font-semibold">
-            실시간 상담 없음
-          </DarkBox>
-        </li>
+        {rooms.length > 0 ? (
+          rooms.map((room) => (
+            <li key={room.roomId}>
+              <DarkBox className="w-full py-7 text-center text-xs font-semibold">
+                {room.consumerName}
+              </DarkBox>
+            </li>
+          ))
+        ) : (
+          <li>
+            <DarkBox className="w-full py-7 text-center text-xs font-semibold">
+              실시간 상담 없음
+            </DarkBox>
+          </li>
+        )}
       </ul>
     </div>
   );
