d5a804c2b901d64edd737239d42bf45bd7baf2a9
InGwonKang <tnqkr3494@naver.com>
2025-08-13 16:19:35 +0900

    [#162] fix: 솔루션 상세 페이지 pie chart 디폴트 ui 추가

--- src/entities/product/ui/pie-chart.tsx
+++ src/entities/product/ui/pie-chart.tsx
@@ -8,29 +8,44 @@ import { StatsProps } from '@/views/vendorMy/model/type';
 ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 
 export default function PieChart({ stats }: { stats: StatsProps[] }) {
-  const data = {
-    labels: stats.map((stat) => stat.label),
+  const isAllZero = stats.every((stat) => stat.percentage === 0);
+
+  const defaultData = {
+    labels: ['데이터 없음'],
     datasets: [
       {
         label: '%',
-        data: stats.map((stat) => stat.percentage),
-        backgroundColor: ['#717171'],
+        data: [100],
+        backgroundColor: ['#E5E5E5'],
         borderColor: ['white'],
         borderWidth: 1.5,
       },
     ],
   };
+
+  const chartData = isAllZero
+    ? defaultData
+    : {
+        labels: stats.map((stat) => stat.label),
+        datasets: [
+          {
+            label: '%',
+            data: stats.map((stat) => stat.percentage),
+            backgroundColor: ['#717171'],
+            borderColor: ['white'],
+            borderWidth: 1.5,
+          },
+        ],
+      };
+
   return (
     <div className="relative -translate-y-20">
       <Pie
-        data={data}
+        data={chartData}
         options={{
           cutout: '55%',
           layout: {
-            padding: {
-              left: 100,
-              right: 100,
-            },
+            padding: { left: 100, right: 100 },
           },
           responsive: true,
           plugins: {
@@ -45,26 +60,17 @@ export default function PieChart({ stats }: { stats: StatsProps[] }) {
               formatter: (value, context) => {
                 const label =
                   context.chart.data.labels?.[context.dataIndex] || '';
-                return `${label}\n${value}%`;
+                return isAllZero ? '데이터 없음' : `${label}\n${value}%`;
               },
               color: 'white',
               backgroundColor: '#23224C',
               textAlign: 'center',
-              padding: {
-                left: 10,
-                right: 10,
-                top: 4.5,
-                bottom: 4.5,
-              },
+              padding: { left: 10, right: 10, top: 4.5, bottom: 4.5 },
               borderRadius: 5,
               anchor: 'end',
               align: 'center',
               clip: false,
-              font: {
-                size: 12,
-                weight: 'bold',
-                lineHeight: 1.5,
-              },
+              font: { size: 12, weight: 'bold', lineHeight: 1.5 },
             },
           },
         }}
